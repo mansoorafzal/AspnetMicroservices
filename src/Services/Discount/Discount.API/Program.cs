0f4b64f3a81a507154d18306f1cb521f4146ef24
mansoorafzal <mansoorafzal@gmail.com>
2021-06-19 16:27:12 +0400

    Discount.API implementation

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,24 @@
+using Discount.API.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Discount.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            host.MigrateDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
