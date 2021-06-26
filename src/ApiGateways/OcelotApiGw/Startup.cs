5dde7a518846efe9a5652270a088d2d62208fe18
mansoorafzal <mansoorafzal@gmail.com>
2021-06-26 18:26:28 +0400

    Ocelot Cache Configuration

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 namespace OcelotApiGw
 {
@@ -14,7 +15,7 @@ namespace OcelotApiGw
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
